30fab208ed799217cb979e5692a68e41032a38f9
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-01-22 16:10:14 +0700

    update liff id

--- liff.js
+++ liff.js
@@ -134,7 +134,7 @@ function makeErrorMsg(errorObj) {
 // -------------- //
 
 function initializeApp() {
-    liff.init({ liffId: "1655587237-kDwyYzoX" },() => initializeLiff(), error => uiStatusError(makeErrorMsg(error), false));
+    liff.init({ liffId: "1655600021-7ENMXj9b" },() => initializeLiff(), error => uiStatusError(makeErrorMsg(error), false));
 }
 
 function initializeLiff() {
